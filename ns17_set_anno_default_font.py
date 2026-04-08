9c57a4bb555542c492071a1cde68082cc79849a6
Dan Sturm <dan@sandwich.co>
2026-04-07 22:00:53 -0700

    set font size too

--- ns17_set_anno_default_font.py
+++ ns17_set_anno_default_font.py
@@ -3,7 +3,8 @@ from hiero.core import events, AnnotationText
 def set_annotation_font(event): 
     text_annotations = [element for element in event.annotation.elements() if isinstance(element, AnnotationText)] 
     for t in text_annotations: 
-        if t.fontFamily() == "Verdana": #the default font family name 
-            t.setFontFamily("SF Mono") #new font family 
+        if t.fontFamily() == "Verdana":
+            t.setFontFamily("SF Mono")
+            t.setFontSize(36)
 
 events.registerInterest("kAnnotationChanged", set_annotation_font)
